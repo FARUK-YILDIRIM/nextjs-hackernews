eac954a2d573b23040911302de6d470275bfa5e0
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-11-29 19:57:52 +0300

    nested comments

--- components/Comment.js
+++ components/Comment.js
@@ -5,7 +5,47 @@ const Comment = ({ comment }) =>
       <div
         className="comment-content"
         dangerouslySetInnerHTML={{ __html: comment.content }}
-      ></div>
+      />
+      {comment.comments && (
+        <div className="nested-comments">
+          {comment.comments.map((nestedComment) => (
+            <Comment key={nestedComment.id} comment={nestedComment} />
+          ))}
+        </div>
+      )}
+
+      <style jsx>{`
+        .comment {
+            margin-bottom: 1.5em;
+
+        }
+        .comment-user{
+            font-size: 0.9rem;
+            font-wight: bold;
+            margin-bottom:0.5em;
+        }
+        .comment-content {
+            font-size: 0.9rem;
+        }
+        .comment-content p{
+            margin:0;
+            margin-bottom;0.5em;
+            word-wrap: break-word;
+        }
+        .comment-content a {
+            color: #f60;
+            text-decoration: underline;
+        }
+        .comment-content pre {
+            max-wight: 100%;
+            overflow: scroll;
+        }
+        .nested-comments {
+            margin-top:1em;
+            border-left:1px solid rgb(0,0,0,0.1);
+            padding-left: 1em;
+        }
+      `}</style>
     </div>
   );
 
