8ed33fa499afe2d8bcd6543807cfd862f6dc7be2
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-11-29 15:33:38 +0300

    errors handling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 import fetch from "isomorphic-fetch";
+import Error from "next/error";
 
 class Index extends React.Component {
   static async getInitialProps() {
-    const responce = await fetch("https://api.hackerwebapp.com/news?page=2");
-    const stories = await responce.json();
+    let stories;
+    try {
+      const responce = await fetch(
+        "https://api.hackerwebapp.com/fy/news?page=2"
+      );
+      stories = await responce.json();
+      return { stories };
+    } catch (err) {
+      console.log(err);
+      stories = [];
+    }
+
     return { stories };
   }
 
   render() {
+    const { stories } = this.props;
+
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
+    }
+
     return <div>hacker news</div>;
   }
 }
