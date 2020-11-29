2ed379c21dd9758221ba5d63e26d5a9fc2ba823a
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-11-29 15:06:22 +0300

    isomorphic-fetch

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import fetch from "isomorphic-fetch";
 
 class Index extends React.Component {
+  static async getInitialProps() {
+    const responce = await fetch("https://api.hackerwebapp.com/news?page=2");
+    const stories = await responce.json();
+    return { stories };
+  }
+
   render() {
     return <div>hacker news</div>;
   }
